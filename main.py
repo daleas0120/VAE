87fbb1d6a1b9198ab6714b2770ba6755b62409b2
William M. Boler <wboler@iupui.edu>
2021-09-24 17:24:36 -0400

    Implements early stopping

--- main.py
+++ main.py
@@ -118,13 +118,35 @@ def main():
         now = "{:%Y%m%dT%H%M}".format(dt.now())
         tb_path = os.path.join(LOG_DIR, now)
 
+    # VAE Instantiation
     vae = VAE(encoder, decoder, WEIGHT_BCE, WEIGHT_SL, WEIGHT_KL, IMG_DIM)
+
+    # Model Compile
     vae.compile(optimizer=keras.optimizers.Adam(learning_rate=LEARNING_RATE))
+
+    # Callbacks
+
+    # Tensorboard
     tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=(tb_path), histogram_freq=10)
 
-    vae.fit(imgs, epochs=EPOCHS, batch_size=MINI_BATCH, callbacks=[tensorboard_callback],
-            use_multiprocessing=True, validation_split=VALIDATION_SPLIT, validation_steps=val_steps,
-            initial_epoch=INITIAL_EPOCH)
+    # EarlyStopping
+    earling_stopping_callback = tf.keras.callbacks.EarlyStopping(
+        monitor='val_loss',
+        min_delta=1E-3,
+        patience=8,
+        verbose=True,
+        mode='auto',
+        restore_best_weights=False
+    )
+
+    # Allow early-terminate by CTRL+C
+    try:
+        # Execute Fitting
+        vae.fit(imgs, epochs=EPOCHS, batch_size=MINI_BATCH, callbacks=[tensorboard_callback, early_stopping_callback],
+                use_multiprocessing=True, validation_split=VALIDATION_SPLIT, validation_steps=val_steps,
+                initial_epoch=INITIAL_EPOCH)
+    except KeyboardInterrupt:
+        print('Terminating training')
 
     # Save network for future use
     encoder.save(filepath=(os.path.join(tb_path, "encoder")))
